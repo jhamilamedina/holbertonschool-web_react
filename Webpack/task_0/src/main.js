302d6bca3a7f5aee2c46534d33af90ab40a1f427
Maly <jhamilamedina@gmail.com>
2024-05-13 21:30:41 -0500

    feat: Crear una configuracion basica en webpack

--- Webpack/task_0/src/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-import $ from 'jquery';
-
-$(document).ready(function() {
-    // Agregar elementos al cuerpo de la página usando jQuery
-    $('body').append('<h1>Hello, World!</h1>');
-    $('body').append('<p>This is a paragraph added using jQuery.</p>');
-});
