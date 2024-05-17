0215b81c212f350dd681f6dc75f6219bc93c0e3d
Maly <jhamilamedina@gmail.com>
2024-05-17 12:09:09 -0500

    fix: Aplicacion basica de React

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,27 +1,20 @@
-import './logoholbi.png';
+import logo from './logo_holbi.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Header div con la clase App-header */}
       <header className="App-header">
-        {/* Logo de Holberton */}
-        <img src="logoholbi.png" alt="" />
-        {/* Título h1 con el texto "School dashboard" */}
+        <img src={logo} alt="holberton logo" />
         <h1>School dashboard</h1>
       </header>
 
-      {/* Body div con la clase App-body */}
-      <div className="App-body">
-        {/* Párrafo con el texto "Login to access the full dashboard" */}
+      <body className="App-body">
         <p>Login to access the full dashboard</p>
-      </div>
+      </body>
 
-      {/* Footer div con la clase App-footer */}
       <footer className="App-footer">
-        {/* Párrafo con el texto "Copyright 2024 - Holberton School" */}
-        <p>Copyright 2024 - Holberton School</p>
+        <p>Copyright 2020 - Holberton School</p>
       </footer>
     </div>
   );
