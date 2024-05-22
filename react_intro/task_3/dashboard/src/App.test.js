d53123d506fd766b45d23b8df691934fa967a9dc
Maly <jhamilamedina@gmail.com>
2024-05-21 23:26:46 -0500

    Test: Crear pruebas de reaccion

--- react_intro/task_3/dashboard/src/App.test.js
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,26 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a div with class App-header', () => {
+  render(<App />);
+  const appHeader = screen.getByText('School dashboard');
+  expect(appHeader).toBeInTheDocument();
+});
+
+test('renders a div with class App-body', () => {
+  render(<App />);
+  const appBody = screen.getByText('Login to access the full dashboard');
+  expect(appBody).toBeInTheDocument();
+});
+
+test('renders a div with class App-footer', () => {
+  render(<App />);
+  const appFooter = screen.getByText(/Copyright 2024 - Holberton School/);
+  expect(appFooter).toBeInTheDocument();
 });
