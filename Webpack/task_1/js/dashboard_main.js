929ea39e8fb989298d406b672b6203dfd30b23b6
Maly <jhamilamedina@gmail.com>
2024-05-22 21:15:20 -0500

    fix:Aprender a usar webpack con un archivo de configuracion

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+const $ = require('jquery');
+const lodash = require('lodash');
+// import $ from 'jquery';
+
+$(document).ready(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('button').on('click', lodash.debounce(updateCounter, 500));
+});
