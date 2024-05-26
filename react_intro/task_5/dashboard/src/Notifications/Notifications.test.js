9c1a017cd4261354c7055f91db1e99c33ba052e1
Maly <jhamilamedina@gmail.com>
2024-05-26 14:13:01 -0500

    Test: Instalar Jest, Enzyme para los test

--- react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { shallow } from 'enzyme';
 import Notifications from "./Notifications";
 
 test("render without crashing", () => {
-  render(<Notifications />);
+  shallow(<Notifications />);
 });
 
 test('renders three list items', () => {
-  render(<Notifications />);
+  shallow(<Notifications />);
   const listItems = screen.getAllByRole('listitem');
   expect(listItems).toHaveLength(3);
 });
 
 test('renders the text "Here is the list of notifications"', () => {
-  render(<Notifications />);
+  shallow(<Notifications />);
   const paragraphElement = screen.getByText('Here is the list of notifications');
   expect(paragraphElement).toBeInTheDocument();
   
