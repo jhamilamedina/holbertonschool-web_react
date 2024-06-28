759cb360fa3e3e84aa9a08434f67b0835501ecb8
Maly <jhamilamedina@gmail.com>
2024-06-27 20:20:24 -0500

    test: Corrigiendo errores en los test

--- React_component/task_1/dashboard/config/setupTests.js
+++ React_component/task_1/dashboard/config/setupTests.js
@@ -5,16 +5,5 @@
 // import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// import { TextEncoder } from 'util';
-
 
 configure({ adapter: new Adapter() });
-
-// Mock the window.alert function
-global.alert = jest.fn();
-
-// Polyfill for TextEncoder
-if (typeof TextEncoder === 'undefined') {
-  const { TextEncoder } = require('util');
-  global.TextEncoder = TextEncoder;
-}
